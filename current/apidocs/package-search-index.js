b526e02ccfe04a14039eb6836a1f7f2ef46613e7
keithboone <keithboone@users.noreply.github.com>
2024-07-04 23:56:29 +0000

    deploy: 47a45023a7cf8985460a95a06f989782cc1b2cd5

---
+++ current/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"gov.cdc.izgw.v2tofhir"},{"l":"gov.cdc.izgw.v2tofhir.converter"},{"l":"gov.cdc.izgw.v2tofhir.datatype"},{"l":"gov.cdc.izgw.v2tofhir.segment"},{"l":"gov.cdc.izgw.v2tofhir.utils"}];updateSearchResults();
